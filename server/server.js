d01cf21a72a3b3134b4e198aba467f1816bf5390
Korey Cheshire <korey.cheshire@gmail.com>
2022-01-13 14:23:42 -0800

    required Apollo Server to server.js

--- server/server.js
+++ server/server.js
@@ -2,9 +2,21 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+//requires apollo server here, do I need the rest above?
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//applying apollo server to express server as middleware
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+
+  context: authMiddleware,
+})
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
